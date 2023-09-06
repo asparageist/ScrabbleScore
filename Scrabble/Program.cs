f37f0a5a8c834d841f6fc857705bf13b46458fdb
Joseph Murray <josephwmurray@gmail.com>
2023-09-05 20:45:13 -0700

    finalized console app functionality

--- Scrabble/Program.cs
+++ Scrabble/Program.cs
@@ -8,9 +8,16 @@ namespace Scrabble
 {
   class Program
   {
-    static void Main() 
+    static void Main()
     {
-      
+      Console.WriteLine("Welcome to Scrabble Score!");
+      Console.WriteLine("Please enter a word to score!");
+
+      string userWord = Console.ReadLine();
+      ScoreChecker checker = new ScoreChecker();
+      var scoreTotal = checker.CalculateScore(userWord);
+
+      Console.WriteLine($"Your score is {scoreTotal}.");
     }
   }
 }
